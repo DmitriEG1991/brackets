eb531650467ea272c5ea26a4e957fbd87f44e60a
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-02-27 22:23:48 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig){
+    let mass = [];
+    let a = ["(", "{", "["];
+    let b = [")", "}", "]"];
+    let c = ["|"];
+    bracketsConfig.forEach(function(abc){
+        if(abc[0] != abc[1]){
+            a.push(abc[0]);
+            b.push(abc[1]);
+        }else{ c.push(abc[0]);
+        }
+    })
+    let str2 = str.toString().split("");
+    for (let i = 0; i < str2.length; i++) {
+        if (a.includes(str2[i])) {
+            mass.push(str2[i]);
+        } else if (b.includes(str2[i])) {
+            if (a.indexOf(mass[mass.length - 1]) == b.indexOf(str2[i])) {
+                mass.pop();
+            } else {
+                return false;
+            }
+        } else if (c.includes(str2[i])) {
+            if (mass[mass.length - 1] == str2[i]) {
+                mass.pop();
+            } else if (!mass.includes(str2[i])) {
+                mass.push(str2[i]);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return mass.length == 0 ? true : false;
+}  
